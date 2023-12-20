c631c27e73ed8a6f7818600f1cb4eb77d4a5ed4c
jep <n.siomusic@gmail.com>
2023-12-20 20:32:19 +0700

    register

--- lib/widgets/nav_bottom.dart
+++ lib/widgets/nav_bottom.dart
@@ -13,56 +13,6 @@ class NavigationBarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return NavigationBar(
-
-        onDestinationSelected: (int index) {
-          // Handle the selected index if needed
-          switch(index) {
-            case 0:
-              Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
-              break;
-            case 1:
-              Navigator.push(context, MaterialPageRoute(builder: (context) => BooklistPage()));
-              break;
-            case 2:
-              Navigator.push(context, MaterialPageRoute(builder: (context) => StoryListPage()));
-              break;
-            case 3:
-              Navigator.push(context, MaterialPageRoute(builder: (context) => ForumPage()));
-              break;
-            case 4:
-              Navigator.push(context, MaterialPageRoute(builder: (context) => LeaderPage()));
-              break;
-            case 5:
-              Navigator.push(context, MaterialPageRoute(builder: (context) => ProfilePage()));
-              break;
-          }
-        },
-        destinations: const <Widget>[
-          NavigationDestination(
-            icon: Icon(Icons.notifications_sharp),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.book_outlined),
-            label: 'Library',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.book_outlined),
-            label: 'Writers Jam',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.forum_rounded),
-            label: 'Forum',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.bar_chart_rounded),
-            label: 'Leaderboard',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
       onDestinationSelected: (int index) {
         // Handle the selected index if needed
         switch (index) {
